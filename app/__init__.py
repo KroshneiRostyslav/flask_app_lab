9e397e240dcafb21cf1943be79ef6660c9714684
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2025-12-05 20:59:23 +0200

    Add products blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_pyfile('..\\config.py')
+app.config.from_pyfile(r'..\config.py')
 
-from . import routes
+from .users import users_bp
+from .products import products_bp
+app.register_blueprint(users_bp)
+app.register_blueprint(products_bp)
---
+++ app/users/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+users_bp = Blueprint(
+    "users",
+    __name__,
+    url_prefix="/",
+    template_folder="templates"
+)
+
+from . import views
