61d71ff515c26f8955ae03adb49235a06f20d7d1
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2025-12-25 15:17:27 +0200

    Add ability to manage cookies in profile page

--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, flash, session
+from flask import render_template, request, redirect, url_for, flash, session, make_response
 from . import users_bp
 
 @users_bp.route('/hi/<string:name>')
@@ -35,16 +35,59 @@ def login():
 
     return render_template('users/login.html')
 
-@users_bp.route('/profile')
+@users_bp.route('/profile', methods=['GET', 'POST'])
 def profile():
     if 'user' not in session:
         flash('Please login first', 'danger')
-        return redirect(url_for('users.login')) 
-    
+        return redirect(url_for('users.login'))
+
     username = session['user']
 
-    return render_template('users/profile.html', username = username)
+    if request.method == 'POST':
+        action = request.form.get('action')
+        response = redirect(url_for('users.profile'))
+        
+        if action == 'add':
+            key = request.form.get('key')
+            value = request.form.get('value')
+            max_age = request.form.get('max_age')
 
+            if not key:
+                flash("Введіть ключ для кукі", "danger")
+                return response
+
+            response.set_cookie(
+                key,
+                value = str(value) if value else "",
+                max_age=int(max_age) if max_age else None
+            )
+            flash(f"Cookie '{key}' успішно додано", 'success')
+            return response
+
+        elif action == 'delete_one':
+            key = request.form.get('key')
+
+            if not key:
+                flash("Введіть ключ для кукі", "danger")
+                return response
+            
+            if key in request.cookies:
+                response.delete_cookie(key)
+                flash(f"Cookie '{key}' видалено", 'success')
+                return response
+            else:
+                flash(f"Cookie '{key}' не знайдено", 'warning')
+                return response
+
+        elif action == 'delete_all':
+            for key in request.cookies.keys():
+                if key != 'session': 
+                    response.delete_cookie(key)
+            flash("Всі cookie видалено", 'success')
+            return response
+
+    return render_template("users/profile.html", username=username, cookies=request.cookies)
+    
 @users_bp.route('/logout')
 def logout():
     session.pop('user', None)
