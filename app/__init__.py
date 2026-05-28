401dd9fb03271cd18dbc10bc04639f25d71714aa
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-05-29 00:47:07 +0300

    change import of config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 app.secret_key = b'secret_key'
 
-app.config.from_pyfile(r'..\config.py')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config.from_pyfile(os.path.join(basedir, "..", "config.py"))
 
 @app.route('/')
 def home():
