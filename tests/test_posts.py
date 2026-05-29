45943448f9d12c70a97870c47f43a23ce5642257
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-05-29 14:26:46 +0300

    Update tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -3,6 +3,7 @@ import unittest
 from app import create_app, db
 from app.posts.models import Post
 
+
 class PostTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -14,27 +15,62 @@ class PostTestCase(unittest.TestCase):
         with self.app.app_context():
             db.create_all()
 
+            post = Post(
+                title="Existing post",
+                content="Existing content",
+                category="news"
+            )
+
+            db.session.add(post)
+            db.session.commit()
+
     def tearDown(self):
 
         with self.app.app_context():
+            db.session.remove()
             db.drop_all()
 
+    # CREATE
     def test_create_post(self):
 
         response = self.client.post(
-            "/post/create",
+            "/posts/post/create",
             data={
-                "title": "Test",
+                "title": "Test title",
                 "content": "Test content",
-                "category": "news"
+                "category": "tech"
             },
             follow_redirects=True
         )
 
         self.assertEqual(response.status_code, 200)
 
+    # ALL POSTS
     def test_all_posts(self):
 
-        response = self.client.get("/post")
+        response = self.client.get("/posts/post")
 
         self.assertEqual(response.status_code, 200)
+
+    # DETAIL POST
+    def test_detail_post(self):
+
+        response = self.client.get("/posts/post/1")
+
+        self.assertEqual(response.status_code, 200)
+
+    # DELETE POST
+    def test_delete_post(self):
+
+        response = self.client.post(
+            "/posts/post/1/delete",
+            follow_redirects=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        with self.app.app_context():
+
+            post = db.session.get(Post, 1)
+
+            self.assertIsNone(post)
